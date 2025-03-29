ed325453ce87910e4abc49e0fe74d69e7071ee2a
Dexter Turner <dexter_p_turner@icloud.com>
2025-03-29 16:29:49 +0000

    half user sign in / out

--- client.py
+++ client.py
@@ -20,6 +20,10 @@ import socket
 import time
 import logging
 import json
+import select
+import getpass
+import hashlib
+import re
 
 logging.basicConfig(
     level=logging.DEBUG,  # Set the logging level
@@ -27,9 +31,10 @@ logging.basicConfig(
 	datefmt='%d/%b/%y | %I:%M:%S%p',
     handlers=[logging.FileHandler("log.log"), logging.StreamHandler()],
 )
-
-host = "0.0.0.0"
-port = 6969
+VERSION = "0.0.1"
+HOST = "0.0.0.0"
+PORT = 6969
+CLIENTNAME = socket.gethostname()
 
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -59,54 +64,137 @@ def enterServerPort() -> int:
   print("Leave blank for port 6969")
   serverPort = input("Server Port:")
   if serverPort == "" or serverPort == " ":
-    serverPort == 6969
+    serverPort = 6969
   try:
     serverPort = int(serverPort)
-  except(TypeError):
+  except(ValueError):
     serverPort = 6969
     logging.critical("Invalid server port given, defaulting to 6969.")
   return serverPort
 #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def isServerOnline(host, port, timeout=2):
+def isServerOnline(HOST, PORT, timeout=2):
   """
   The function will deal with changing the port to a pythonChatApp UDP port
-  Check if the server is online AND if it registers as a pythonChatApp server
-  Also checks if the client (this) and server share the same MAJOR version
-  Warns if the server is a lower MINOR version
+  Check if the server is online AND checks if the client (this) and server share the same MAJOR version
+  If the server has a lower MINOR version connection isn't allowed
   """
   temporaryClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   temporaryClientSocket.settimeout(timeout)  # Set timeout for response
-  logging.info(f"Checking {host}, {port}")
+  logging.info(f"Checking {HOST}, {PORT}")
   try:
-    # Send an empty "ping" message
-    temporaryClientSocket.sendto(b"PING", (host, port+1))
-
-    # Wait for response
+    dict = {"clientName": CLIENTNAME, "clientVersion": VERSION}
+    dict = json.dumps(dict).encode('utf-8')
+    temporaryClientSocket.sendto(dict, (HOST, PORT+1))
+    
     data, _ = temporaryClientSocket.recvfrom(1024)
-
-    # {"serverName": "X", "serverVersion": "0.0.1", ""}
+    # {"serverName": "X", "serverVersion": "0.0.1"}
     data = json.loads(data.decode('utf-8'))
 
-    if data['serverVersion'].split(".")[0] == "0":
-      logging.info("MAJOR OK")
-    if data['serverVersion'].split(".")[1] == "0":
-      logging.info("MINOR OK")
-    if data['serverVersion'].split(".")[2] == "1":
-      logging.info("PATCH OK")
-    if data == b"PONG":
-        logging.info("Server confirmed")
-        return True
+    if not int(data['serverVersion'].split(".")[0]) == int(VERSION.split(".")[0]):
+      logging.critical(f"Server is incompatible with client. (S: {data['serverVersion']}, C: {VERSION})\nServer is {int(data['serverVersion'].split(".")[0]) - int(VERSION.split(".")[0])} MAJOR versions ahead. (- if behind)")
+      return False
+    elif not int(data['serverVersion'].split(".")[1]) <= int(VERSION.split(".")[1]):
+      logging.critical(f"Server is incompatible with client. (S: {data['serverVersion']}, C: {VERSION})\nServer is {int(data['serverVersion'].split(".")[1]) - int(VERSION.split(".")[1])} MINOR versions ahead. (- if behind)")
+      return False
+    else:
+      logging.info(f"Server is online and compatible with client. (S: {data['serverVersion']}, C: {VERSION})\nServer is {int(data['serverVersion'].split(".")[0]) - int(VERSION.split(".")[0])} MAJOR versions ahead\nServer is {int(data['serverVersion'].split(".")[0]) - int(VERSION.split(".")[0])} MINOR versions ahead\nServer is {int(data['serverVersion'].split(".")[2]) - int(VERSION.split(".")[2])} PATCH versions ahead")
+      return True
+  
   except socket.timeout:
+    logging.warning(f"Server didn't respond, this likely means the server is not online or accessible.")
     return False
   except Exception as e:
-    print(f"Error: {e}")
+    logging.warning(f"Error: {e}")
     return False
   finally:
     temporaryClientSocket.close()
 
-isServerOnline(host, port)
-clientSocket.connect((host, port))
-time.sleep(0.1)
-print(socket.gethostname())
-clientSocket.close()
+def getCredentials():
+  username = input("Username:")
+  password = hashlib.md5(getpass.getpass(prompt="Password:").encode()).hexdigest()
+  return username, password
+
+def getSignupCredentials():
+  while True:
+    username = input("Username:")
+    password = hashlib.md5(getpass.getpass(prompt="Password:").encode()).hexdigest()
+    confirmPassword = hashlib.md5(getpass.getpass(prompt="Confirm Password:").encode()).hexdigest()
+    if not password == confirmPassword:
+      print("Passwords do not match.")
+    else:
+      break
+  email = input("(OPTIONAL) Email:")
+  if email == "" or email == " " or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+    email = None
+  return username, password, email
+
+def dictToBin(dict):
+  return json.dumps(dict).encode('utf-8')
+
+def binToDict(bin):
+   return json.loads(bin.decode('utf-8'))
+
+def waitForResponse(sock, timeout=10):
+  startTimeStamp = time.time()
+  while startTimeStamp < time.time()+timeout:
+    ready, _, _ = select.select([sock], [], [], 0)
+    if ready:
+      return True
+  else:
+    return False
+
+
+def clientStart(clientSocket):
+  
+  while True:
+    username = "anon"
+    password = ""
+    print("""1. Login\n2. signup\n3. Anon""")
+    userChoice = input(">")
+    if userChoice == "1":
+      username, password = getCredentials()
+      dict = {"type": "credentials", "credentialsType": "login", "username": username, "password": password}
+      clientSocket.sendall(dictToBin(dict))
+      waitForResponse(clientSocket)
+      data = binToDict(clientSocket.recv(1024))
+      if data["type"] == "confirm":
+        break
+      else:
+        print(f"Fail: f{data["message"]}")
+    elif userChoice == "2":
+      username, password, email = getSignupCredentials()
+      dict = {"type": "credentials", "credentialsType": "signup", "username": username, "password": password, "email": email}
+      clientSocket.sendall(dictToBin(dict))
+      waitForResponse(clientSocket)
+      data = binToDict(clientSocket.recv(1024))
+      if data["type"] == "confirm":
+        break
+      else:
+        print(f"Fail: f{data["message"]}")
+    elif userChoice == "3":
+      print("Anon users have limited permissions")
+      dict = {"type": "credentials", "credentialsType": "anon"}
+      clientSocket.sendall(dictToBin(dict))
+      waitForResponse(clientSocket)
+      data = binToDict(clientSocket.recv(1024))
+      if data["type"] == "confirm":
+        break
+      else:
+        print(f"Server reported a failure with the following message:\n{data["message"]}")
+    else:
+      None
+  logging.info(f"Logged in as {username}")
+  # ready, _, _ = select.select([clientSocket], [], [], 0)
+
+HOST = enterServerAddress()
+PORT = enterServerPort()
+if isServerOnline(HOST, PORT):
+  clientSocket.connect((HOST, PORT))
+
+  clientStart(clientSocket)
+  time.sleep(0.1)
+  print()
+  clientSocket.close()
+else:
+  exit()
--- server.py
+++ server.py
@@ -8,26 +8,100 @@ import socket
 import threading
 import time
 import json
-
+import select 
+import os
+import base64
 logging.basicConfig(
     level=logging.DEBUG,  # Set the logging level
     format="[SERVER] [%(asctime)s.%(msecs)03d | %(created)f] [%(filename)s@%(funcName)s@%(lineno)d] [%(levelname)s] %(message)s",
 	datefmt='%d/%b/%y | %I:%M:%S%p',
     handlers=[logging.FileHandler("log.log"), logging.StreamHandler()],
 )
+VERSION = "0.0.1"
 HOST = "0.0.0.0"
 TCP_PORT = 6969
 UDP_PORT = TCP_PORT+1 # To keep the program simple the UDP can be by standard 1 integer off the TCP
 SERVERNAME = "Alpha"
+
+os.chdir("serverData")
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # ALlow reusing the same host
 serverSocket.bind((HOST, TCP_PORT))
 serverSocket.listen()
 logging.info(f"\nClients only need your hostname and TCP Port\nServer listening on\n{HOST} @ {TCP_PORT} (UDP is {UDP_PORT})\n\nOR\n\n{socket.gethostbyname(socket.gethostname())} @ {TCP_PORT} (UDP is{UDP_PORT})")
+
+def isSocketOpen(sock):
+    try:
+        # Peek at data without removing it from the buffer
+        data = sock.recv(1, socket.MSG_PEEK)
+        return bool(data)  # If data is received, the socket is open
+    except socket.error:
+        return False  # If an error occurs, the socket is closed
+
+def dictToBin(dict):
+  return json.dumps(dict).encode('utf-8')
+
+def toDict(bin):
+   return json.loads(bin.decode('utf-8'))
+
+def getUserInfo(username):
+  username = base64.urlsafe_b64encode(bytes(username, encoding='ascii'))
+  if not username in os.listdir():
+     return False
+  with open(username, "r") as f:
+     return f.read()
+
+def createUser(username, password, email=None):
+  username = base64.urlsafe_b64encode(bytes(username, encoding='ascii'))
+  if username in os.listdir():
+      return False
+  with open(username, "w") as f:
+    f.write(str({"password": password, "email": email}))
+    return True
+
 def handleClient(clientSocket, addr):
-    clientSocket.sendall(b'Hey')
-    clientSocket.close()
+    signedIn = False
+    anonymous = False
+    username = None
+    password = None
+    email = None
+    while True:
+      if not isSocketOpen(clientSocket):
+        return False
+      ready, _, _ = select.select([clientSocket], [], [], 0)
+      if ready:
+          data = toDict(clientSocket.recv(1024))
+          logging.info(f"[RAW DATA] ({addr}) {data}")
+          if data["type"] == "credentials":
+            if not signedIn:
+              if data["credentialsType"] == "login":
+                anonymous, username, password = False, data["username"], data["password"]
+                userInfo = getUserInfo(username)
+                if not userInfo:
+                  clientSocket.sendall(dictToBin({"type": "fail", "message": f"{addr} tried to log in as a user that either doesn't exist or using the wrong password."}))
+                elif not password == userInfo["password"]:
+                  clientSocket.sendall(dictToBin({"type": "fail", "message": f"{addr} tried to log in as a user that either doesn't exist or using the wrong password."}))
+                else:
+                  clientSocket.sendall(dictToBin({"type": "confirm"}))
+                  signedIn = True
+              elif data["credentialsType"] == "signup":
+                anonymous, username, password, email = False, data["username"], data["password"], data["email"]
+                if not createUser(username, password, email):
+                  clientSocket.sendall(dictToBin({"type": "fail", "message": f"{addr} tried to signup as a user that already exists"}))
+                else:
+                  clientSocket.sendall(dictToBin({"type": "confirm"}))
+                  signedIn = True
+              elif data["credentialsType"] == "anon":
+                signedIn, anonymous, username, password, email = True, True, None, None, None
+                clientSocket.sendall(dictToBin({"type": "confirm"}))
+                signedIn = True
+            else:
+               clientSocket.sendall(dictToBin({"type": "fail", "message": f"{addr} is already logged in"}))
+               logging.warning(f"{addr} tried to re-login")
+          else:
+             logging.warning(f"Malformed message from {addr},\n{data}")
+
     return
 
 def udpOnlineCheck():
@@ -39,11 +113,14 @@ def udpOnlineCheck():
   while True:
       try:
           data, addr = udp_socket.recvfrom(1024)  # Receive UDP message
-          if data == b"PING":
-              test_dict = {"serverName": "X", "serverVersion": "0.0.1"}
+
+          data = json.loads(data.decode('utf-8'))
+
+          if data:
+              test_dict = {"serverName": SERVERNAME, "serverVersion": VERSION}
               JSON = json.dumps(test_dict).encode('utf-8')
               udp_socket.sendto(JSON, addr)  # Send response
-              logging.info(f"Responded to PING from {addr}")
+              logging.info(f"Responded to udpOnlineCheck from {addr}({data["clientName"]})")
       except Exception as e:
           logging.error(f"UDP error: {e}")
     
