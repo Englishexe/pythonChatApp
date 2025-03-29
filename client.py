975033c7dc983ceef9111c54df1974cc934e89a0
Dexter Turner <dexter_p_turner@icloud.com>
2025-03-29 09:36:44 +0000

    Created client & server .py

---
+++ client.py
@@ -0,0 +1,5 @@
+"""
+client.py
+
+The client runs this Python file, connects to a server.py
+"""
---
+++ server.py
@@ -0,0 +1,5 @@
+"""
+server.py
+
+The server runs this Python file, waits for connections from client.py(s)
+"""
